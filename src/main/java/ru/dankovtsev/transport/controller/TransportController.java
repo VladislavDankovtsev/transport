48ef8df9bed569399b27ea135a1f99980ceb655d
Vldaislav_19 <vladislav.dankovtsev@yandex.ru>
2020-04-02 14:27:22 +0300

    проверка соединения с главным сервер приложением

--- src/main/java/ru/dankovtsev/transport/controller/TransportController.java
+++ src/main/java/ru/dankovtsev/transport/controller/TransportController.java
@@ -1,6 +1,7 @@
 package ru.dankovtsev.transport.controller;
 
 import org.springframework.web.bind.annotation.RequestMethod;
+import ru.dankovtsev.transport.model.CheckConnection;
 import ru.dankovtsev.transport.model.Transport;
 import ru.dankovtsev.transport.service.TransportService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,12 @@ public class TransportController {
     @RequestMapping(path = "/online", method = RequestMethod.GET)
     public Transport getTransportNow(){
         return transportServiceConfig.getTransport();
+    }
+
+    @RequestMapping(path = "/check", method = RequestMethod.GET)
+    public CheckConnection getAgricultureConnection(){
+        CheckConnection checkConnection = new CheckConnection();
+        checkConnection.setCheck("work");
+        return checkConnection;
     }
 }
---
+++ src/main/java/ru/dankovtsev/transport/model/CheckConnection.java
@@ -0,0 +1,13 @@
+package ru.dankovtsev.transport.model;
+
+public class CheckConnection {
+    private String check;
+
+    public String getCheck() {
+        return check;
+    }
+
+    public void setCheck(String check) {
+        this.check = check;
+    }
+}
